6f5967e4308a53986e21f577ff357293d3c0e444
royalgameslatam-sketch <royalgameslatam@gmail.com>
2026-02-12 19:45:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -127,6 +127,8 @@ def webhook():
 # =============================
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
